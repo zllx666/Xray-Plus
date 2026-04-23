3511a2f356e643c25c0a49d910dca61f9b9be998
zllx666 <60544614+zllx666@users.noreply.github.com>
2026-04-23 11:56:46 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -686,6 +686,22 @@ do_install() {
     { "protocol": "freedom", "tag": "direct" },
     { "protocol": "blackhole", "tag": "block" }
   ]
+  "routing": {
+  "domainStrategy": "IPOnDemand",
+  "rules": [
+      {
+        "type": "field",
+        "ip": [ "::/0", "0.0.0.0/0" ],
+        "network": "tcp,udp",
+        "outboundTag": "direct"
+      },
+      {
+        "type": "field",
+        "network": "tcp,udp",
+        "outboundTag": "direct"
+      }
+    ]
+  }
 }
 EOF
   ok "配置写入完成"
